524c138c12457ae72a16630a35a57373fdd48f77
Hoppi164 <Hoppi164@gmail.com>
2023-03-07 22:28:40 +1000

    Basic Implementation of Card

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,27 @@
+<script>
+	/** @type {string} */
+	export let task;
+
+	/** @type {string} */
+	export let points;
+
+	/** @type {boolean} */
+	export let isRepeatingTask = false;
+
+	/** @type {string} */
+	export let image;
+</script>
+
+<div>
+	<div class="cardHeader" />
+	<div class="cardFooter">
+		<h1>{task}</h1>
+		<div class="grid">
+			<span>#{points}</span>
+			<span>{isRepeatingTask}</span>
+		</div>
+	</div>
+</div>
+
+<style>
+</style>
